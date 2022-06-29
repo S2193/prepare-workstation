45e4a4c3cfa6adc24764721a70cb5ab894f871a0
Sceva Jordan <sceva@Scevas-Air.attlocal.net>
2022-06-29 11:20:44 -0400

    updated

--- azure/main.tf
+++ azure/main.tf
@@ -4,14 +4,14 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "example" {
   name     = "lwplabs-rg-docker"
-  location = "eastus"
+  location = "westus3"
 }
 
 module "linuxservers" {
   source              = "Azure/compute/azurerm"
   resource_group_name = azurerm_resource_group.example.name
   vm_os_simple        = "UbuntuServer"
-  public_ip_dns       = ["linsimplevmips"] // change to a unique name per datacenter region
+  #public_ip_dns       = ["linsimplevmips"] // change to a unique name per datacenter region
   vnet_subnet_id      = module.network.vnet_subnets[0]
 
   depends_on = [azurerm_resource_group.example]
