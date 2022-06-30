6d8f11fddc877fc0d8ff3423a253de60ddf31935
Sceva Jordan <sceva@Scevas-Air.attlocal.net>
2022-06-30 12:40:23 -0400

    updated

--- azure/main.tf
+++ azure/main.tf
@@ -3,7 +3,11 @@ variable "group_name" {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+      resource_group {
+       prevent_deletion_if_contains_resources = false
+     }
+}
 }
 
 resource "random_id" "value" {
