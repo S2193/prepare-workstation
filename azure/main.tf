f04a8950198c00e070781e78a1f07b0a6963bbda
Sceva Jordan <sceva@Scevas-Air.attlocal.net>
2022-07-13 20:54:41 -0400

    updated number of server

--- azure/main.tf
+++ azure/main.tf
@@ -1,13 +1,13 @@
 variable "group_name" {
-    type = string
+  type = string
 }
 
 provider "azurerm" {
   features {
-      resource_group {
-       prevent_deletion_if_contains_resources = false
-     }
-}
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
 resource "random_id" "value" {
@@ -15,7 +15,7 @@ resource "random_id" "value" {
 }
 
 locals {
-   template_file_int  = templatefile("./install.tpl", {})
+  template_file_int = templatefile("./install.tpl", {})
 }
 
 resource "azurerm_resource_group" "example" {
@@ -27,13 +27,17 @@ module "linuxservers" {
   source              = "Azure/compute/azurerm"
   resource_group_name = azurerm_resource_group.example.name
   vm_os_simple        = "UbuntuServer"
+  nb_instances        =  var.num_instances
+  nb_public_ip        =  var.num_instances
   #public_ip_dns       = ["linsimplevmips"] // change to a unique name per datacenter region
-  vnet_subnet_id      = module.network.vnet_subnets[0]
+  vnet_subnet_id       = module.network.vnet_subnets[0]
   storage_account_type = "Standard_LRS"
   remote_port          = "22"
   custom_data          = local.template_file_int
-  enable_ssh_key                   = true
+  enable_ssh_key       = true
   ssh_key              = "mykey.pub"
+  delete_os_disk_on_termination = "true"
+  admin_password = "test123"
 
   depends_on = [azurerm_resource_group.example]
 }
@@ -47,6 +51,23 @@ module "network" {
   depends_on = [azurerm_resource_group.example]
 }
 
+resource "azurerm_network_security_rule" "web" {
+  name                        = "allow_remote_web_traffic"
+  resource_group_name         = azurerm_resource_group.example.name
+  description                 = "Allow web traffic"
+  priority                    = 110
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_ranges     = ["80", "8000-8090"]
+  source_address_prefixes     = ["0.0.0.0/0"]
+  destination_address_prefix  = "*"
+  network_security_group_name = "myvm-nsg"
+
+  depends_on = [module.linuxservers.azurerm_network_security_group]
+}
+
 output "linux_vm_public_name" {
-  value = module.linuxservers.public_ip_address
+  value = module.linuxservers.*.public_ip_address
 }
